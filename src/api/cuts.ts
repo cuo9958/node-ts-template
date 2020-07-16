21a6b5163f7316131b1cf93b2e358eaf20cc0bc4
疯狂紫萧 <cuo9958@gmail.com>
2020-07-16 22:34:59 +0800

    Delete cuts.ts

--- src/api/cuts.ts
+++
@@ -1,9 +0,0 @@
-import Router from "../extend/TRouter";
-
-const router = new Router();
-
-router.tget("/", function (ctx, next) {
-    ctx.body = ctx.Success("成功");
-});
-
-export default router.routes();
