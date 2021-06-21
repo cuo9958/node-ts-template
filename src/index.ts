61cd92751019f49916566fbfabfabb24c03d9538
疯狂紫萧 <cuo9958@gmail.com>
2021-06-21 18:10:41 +0800

    去掉无用的中间件

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,10 @@
 import Koa from "koa";
 import KoaBody from "koa-body";
-import { sendSuccess, sendError } from "./middleware/resp";
 import routers from "./router";
 
 const app = new Koa();
 
 app.use(KoaBody({}));
-
-//加载默认的处理方法
-app.use(sendSuccess);
-app.use(sendError);
 
 //加载路由
 app.use(routers.routes()).use(routers.allowedMethods());
